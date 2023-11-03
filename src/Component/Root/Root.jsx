49d055c23329b56cc7fe30926f6c0de3a25a1646
hasib1010 <asifmahmud664@gmail.com>
2023-11-03 20:14:46 +0600

    Project finished

--- src/Component/Root/Root.jsx
+++ src/Component/Root/Root.jsx
@@ -1,19 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
-<<<<<<< HEAD
 import Footer from "../Footer/Footer";
-=======
 import "./Root.css"
->>>>>>> 3199d399567f2a76ae7ba439033e3ca96c2f777c
 
 const Root = () => {
   
     return (
-<<<<<<< HEAD
-        <div>
-=======
         <div className="font">
->>>>>>> 3199d399567f2a76ae7ba439033e3ca96c2f777c
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
